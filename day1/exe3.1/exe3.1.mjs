1749bc86e63885fb2fdb16ee7e1aaa873db8f122
David Bobritsky <dvdbobr@gmail.com>
2021-04-12 00:09:56 +0300

    second commit

---
+++ day1/exe3.1/exe3.1.mjs
@@ -0,0 +1,8 @@
+import {getNotes,sum, subtract} from './utils.js';
+//const getNotes=require('./utils.js');
+const msg = getNotes()
+console.log(msg)
+const result = sum(1,3)
+console.log(result)
+const sub=subtract(3,1)
+console.log(sub)
---
+++ day1/exe3.1/utils.js
@@ -0,0 +1,11 @@
+const getNotes = () => {
+    return "your notes"
+}
+function sum(a, b) {
+    return a + b
+}
+const subtract = (a, b) => {
+    return a - b
+}
+
+export { getNotes, sum, subtract }
